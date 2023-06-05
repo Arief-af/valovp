73fe27163eaf7c878a47888b962e595c15062580
Arief Akhmad Fauzi <sekolah.arief@gmail.com>
2023-06-05 14:34:34 +0700

    add cors

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
+app.use(cors);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const appRoute = require("./src/routes/index");
+app.use("/api", appRoute);
 
-const appRoute = require('./src/routes/index');
-app.use('/api', appRoute);
-
-app.listen(8080, ()=>{
-    console.log('Server Berjalan di Port : 8080');
+app.listen(8080, () => {
+  console.log("Server Berjalan di Port : 8080");
 });
